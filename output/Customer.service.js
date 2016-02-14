72ffcfd88b7432979555d8ae65fda7d1d68983e6
climberchuck <chuck.bridgen@gmail.com>
2016-02-14 13:56:38 -0500

    Added creation of multiple files based on api.json serviceName property.

---
+++ output/Customer.service.js
@@ -0,0 +1,12 @@
+export function GetCompanies () {
+return $http["GET"]("/api/v1/company");
+}
+export function GetCompanyById (companyId) {
+return $http["GET"]("/api/v1/company/"+ companyId);
+}
+export function GetCompanyContactById (companyId, contactId) {
+return $http["GET"]("/api/v1/company/"+ companyId +"/contact/"+ contactId);
+}
+export function CreateCompany (data) {
+return $http["POST"]("/api/v1/company", data);
+}
--- public/main.js
+++ public/main.js
@@ -4,71 +4,79 @@ var apiData = require('./../input/api.json');
 var fs = require('fs');
 console.log("I'm running.");
 
-var fileName = 'output/service.js';
 
-var wstream = fs.createWriteStream(fileName);
-wstream.on('finish', () => {
-    console.log(`Finished creating {fileName}`)
-})
+
+
 var isMock = false;
-_.forEach(apiData, (api) => {
-    var result = api['response-body'];
-    var methodName = api['method-name'];
-    var httpMethod = api['method'];
-    var urlTemplate = api['url-template'];
-    var parameters = parseUrlParameters(urlTemplate);
+var services = _.groupBy(apiData, "serviceName");
+var svcNames = Object.getOwnPropertyNames(services);
+_.forEach(Object.getOwnPropertyNames(services), (serviceName) => {
+    var fileName = 'output/' + serviceName + '.service.js';
+    var wstream = fs.createWriteStream(fileName);
+    writeToStream(services[serviceName], wstream);
 
-    wstream.write(`export function ` + methodName + ` (`);
-    console.log(methodName);
-    console.log(parameters);
-    _.forEach(parameters, (p, index) => {
-        wstream.write(p)
-        if (index != (parameters.length - 1)) {
-            wstream.write(`, `);
+    wstream.on('finish', () => {
+        console.log('Finished creating ' + fileName + '');
+    })
+});
+function writeToStream(apiData, wstream) {
+    _.forEach(apiData, (api) => {
+        var result = api['response-body'];
+        var methodName = api['method-name'];
+        var httpMethod = api['method'];
+        var urlTemplate = api['url-template'];
+        var parameters = parseUrlParameters(urlTemplate);
+
+        wstream.write(`export function ` + methodName + ` (`);
+        _.forEach(parameters, (p, index) => {
+            wstream.write(p)
+            if (index != (parameters.length - 1)) {
+                wstream.write(`, `);
+            }
+        });
+        if (httpMethod === 'POST' || httpMethod === 'PUT') {
+            if (parameters.length == 0) {
+                wstream.write(`data`);
+            }
+            else {
+                wstream.write(`, data`);
+            }
         }
-    });
-    if (httpMethod === 'POST' || httpMethod === 'PUT') {
-        if (parameters.length == 0) {
-            wstream.write(`data`);
+        wstream.write(`) {`);
+        wstream.write(`\n`);
+        if (isMock) {
+            wstream.write('return ');
+            wstream.write(JSON.stringify(result));
+            wstream.write(';\n');
         }
         else {
-            wstream.write(`, data`);
-        }
-    }
-    wstream.write(`) {`);
-    wstream.write(`\n`);
-    if (isMock) {
-        wstream.write('return ');
-        wstream.write(JSON.stringify(result));
-        wstream.write(';\n');
-    }
-    else {
-        var hasBodyData = (httpMethod === 'POST' || httpMethod === 'PUT');
-        var rs = '';
-        rs += 'return $http["';
-        rs += httpMethod;
-        rs += '"]("';
+            var hasBodyData = (httpMethod === 'POST' || httpMethod === 'PUT');
+            var rs = '';
+            rs += 'return $http["';
+            rs += httpMethod;
+            rs += '"]("';
 
-        var url = urlTemplate;
-        _.forEach(parameters, (p, index) => {
-            var replacementText = '"+ ' + p;
-            if (index != (parameters.length - 1)) {
-                replacementText += ' +"';
+            var url = urlTemplate;
+            _.forEach(parameters, (p, index) => {
+                var replacementText = '"+ ' + p;
+                if (index != (parameters.length - 1)) {
+                    replacementText += ' +"';
+                }
+                url = url.replace('{' + p + '}', replacementText);
+            });
+            rs += url;
+            if (parameters.length == 0) { rs += '"'; }
+            if (hasBodyData) {
+                rs += ', data';
             }
-            url = url.replace('{' + p + '}', replacementText);
-        });
-        rs += url;
-        if (parameters.length == 0) { rs += '"'; }
-        if (hasBodyData) {
-            rs += ', data';
+            rs += ');\n';
+            wstream.write(rs);
         }
-        rs += ');\n';
-        wstream.write(rs);
-    }
-    wstream.write('}\n');
-});
+        wstream.write('}\n');
+    });
 
-wstream.end();
+    wstream.end();
+}
 
 function parseUrlParameters(urlTemplate) {
     var results = [], re = /{([^}]+)}/g, text;
